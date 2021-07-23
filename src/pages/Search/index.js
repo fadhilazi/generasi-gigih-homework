5de2e19db337e31b6eb7041d365b6abc22374575
fadhilazi <87027666+fadhilazi@users.noreply.github.com>
2021-07-23 11:15:06 +0700

    Add files via upload

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import style from "./search.module.css";
+import TrackList from "../../components/tracklist/tracklist";
 
 const CLIENT_ID = "a94477da47e340cba87ea9859da1744b";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
@@ -10,7 +11,9 @@ const SCOPE = "playlist-modify-private";
 export default function Search() {
   const [token, setToken] = useState("");
   const [search, setSearch] = useState("");
-  const [datas, setDatas] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+  const [isSelected, setIsSelected] = useState(true);
 
   const getTokenFromUrl = (hash) => {
     const stringAfterHastag = hash.substring(1);
@@ -28,13 +31,15 @@ export default function Search() {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPE}&response_type=token&show_dialog=true`;
   };
 
+  
+
   useEffect(() => {
     if (window.location.hash) {
       const { access_token } = getTokenFromUrl(window.location.hash);
       // console.log(access_token, token_type, expires_in);
       setToken(access_token);
     }
-    console.log(datas);
+    console.log(tracks);
   }, []);
 
   const buttonHandleSearch = () => {
@@ -44,9 +49,9 @@ export default function Search() {
       },
     })
       .then((res) => res.json())
-      .then((data) => setDatas(data.tracks.items));
+      .then((data) => setTracks(data.tracks.items));
   };
-  console.log(datas);
+  console.log(tracks);
 
   return (
     <div className={style["wrapper"]}>
@@ -67,37 +72,19 @@ export default function Search() {
         </button>
       </div>
 
-      <div className={style["wrapper-table"]}>
-        {datas ? (
-          <table className={style["track-list"]}>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Title | Artist</th>
-                <th>Album</th>
-                <th>Release Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {datas.map((data, idx) => {
-                return (
-                  <tr key={data.id}>
-                    <td>{idx + 1}</td>
-                    <td>
-                      <img src={data.album.images[2].url} alt={data.name} />
-                      {data.name}
-                    </td>
-                    <td>{data.album.name}</td>
-                    <td>{data.album.release_date}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+      <div className={style["list-track"]}>
+        {tracks.length > 0 ? (
+          <TrackList
+            tracks={tracks}
+            playlist={playlist}
+            setPlaylist={setPlaylist}
+            isSelected={isSelected}
+            setIsSelected={setIsSelected}
+          />
         ) : (
           <tr>
             <td colSpan="5" style={{ color: "white" }}>
-              Tidak ada Data
+              Search your Music
             </td>
           </tr>
         )}
