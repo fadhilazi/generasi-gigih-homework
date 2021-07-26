5444e711594f78dbcade85a5f6af65bab3a1217a
fadhilazi <87027666+fadhilazi@users.noreply.github.com>
2021-07-26 14:59:51 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Search from "./pages/Search";
 
+import {
+  loginSpotify,
+  getAccessTokenFromURL,
+  getProfile,
+} from "./functions/auth";
+
 function App() {
   return (
     <div className="app">
-      <Search />
+      <div className="content">
+        {/* <Search getAccessTokenFromURL={getAccessTokenFromURL} /> */}
+        <CreatePlaylist
+          getAccessTokenFromURL={getAccessTokenFromURL}
+          getProfile={getProfile}
+        />
+      </div>
     </div>
   );
 }
